1f50ed75f3286fa9c6b9783f9c91af5471cfeaf2
mahmoudzakaria12 <mahmoudzsali247@gmail.com>
2024-09-01 03:42:50 +0300

    Move apps to apps file

---
+++ static/js/script.js
@@ -0,0 +1,160 @@
+const hrefs = [
+		"./apps/calculo_simple_calculator/calculo.html",
+		"./apps/clk_analog_clock/clk_analog_clock.html",
+		"./apps/cloudi_weather_review/cloudi_weather_review.html",
+		"./apps/croppit_image_cropper/croppit_image_cropper.html",
+		"./apps/ding_alarm_clock/ding_alarm_clock.html",
+		"./apps/exchnge_currency_converter/exchnge_currency_converter.html",
+		"./apps/exprot_save_text_as_file/exprot_save_text_as_file.html",
+		"./apps/eztext_text_to_speech/eztext_text_to_speech.html",
+		"./apps/icode_live_code_editor/icode_live_code_editor.html",
+		"./apps/imagz_image_editor/imagz_image_editor.html",
+		"./apps/keypass_password_generator/keypass_password_generator.html",
+		"./apps/klik_detect_key_pressed/klik_detect_key_pressed.html",
+		"./apps/my_clock_X/my_clock_X.html",
+		"./apps/qrify_qr_code_generator/qrify_qr_code_generator.html",
+		"./apps/salat_pray_times/salat_pray_times.html",
+		"./apps/sayinz_random_quotes_generator/sayinz_random_quotes_generator.html",
+		"./apps/scalinng_images_resize/scalinng_images_resize.html",
+		"./apps/scrabz_scrambel_game/scrabz_scrambel_game.html",
+		"./apps/sketshh_painting_webapp/sketshh_painting_webapp.html",
+		"./apps/snapzio_screenshot_capture/snapzio_screenshot_capture.html",
+		"./apps/tabb_typing_test_game/tabb_typing_test_game.html",
+		"./apps/novi/novi.html",
+		"./apps/tranzlate_language_translator/tranzlate_language_translator.html",
+		"./apps/up_vue_video_player/up_vue_video_player.html",
+		"./apps/vi_vid_cash_credit_card/vi_vid_cash_credit_card.html",
+		"./apps/vibs_music_player/vibs_music_player.html",
+		"./apps/wekks_calendar_display/wekks_calendar_display.html",
+		"https://mahmoud46.github.io/Rush_quiz/",
+		"https://mahmoud46.github.io/Mushaf_quran_reader_and_audio_player/",
+		"./apps/ethereye_cryptocurrencies_price_tracker/ethereye.html",
+		"https://mahmoud46.github.io/Sports-Enigineering-Final-Project/",
+	],
+	image_srcs = [
+		"./apps/calculo_simple_calculator/static/imgs/Calculo.svg",
+		"./apps/clk_analog_clock/imgs/fav-icon.svg",
+		"./apps/cloudi_weather_review/imgs/fav-icon.svg",
+		"./apps/croppit_image_cropper/imgs/fav-icon.svg",
+		"./apps/ding_alarm_clock/imgs/fav-icon.svg",
+		"./apps/exchnge_currency_converter/imgs/fav-icon.svg",
+		"./apps/exprot_save_text_as_file/imgs/fav-icon.svg",
+		"./apps/eztext_text_to_speech/imgs/fav-icon.svg",
+		"./apps/icode_live_code_editor/imgs/fav-icon.svg",
+		"./apps/imagz_image_editor/imgs/fav-icon.svg",
+		"./apps/keypass_password_generator/img/fav-icon.svg",
+		"./apps/klik_detect_key_pressed/imgs/fav-icon.svg",
+		"./apps/my_clock_X/imgs/fav-icon.svg",
+		"./apps/qrify_qr_code_generator/imgs/fav-icon.svg",
+		"./apps/salat_pray_times/imgs/fav-icon.svg",
+		"./apps/sayinz_random_quotes_generator/imgs/fav-icon.svg",
+		"./apps/scalinng_images_resize/imgs/fav-icon.svg",
+		"./apps/scrabz_scrambel_game/imgs/fav-icon.svg",
+		"./apps/sketshh_painting_webapp/imgs/fav-icon.svg",
+		"./apps/snapzio_screenshot_capture/imgs/fav-icon.svg",
+		"./apps/tabb_typing_test_game/imgs/fav-icon.svg",
+		"./apps/novi/static/icons/Novi.svg",
+		"./apps/tranzlate_language_translator/imgs/fav-icon.svg",
+		"./apps/up_vue_video_player/imgs/fav-icon.svg",
+		"./apps/vi_vid_cash_credit_card/imgs/fav-icon.svg",
+		"./apps/vibs_music_player/assets/imgs/fav-cion.svg",
+		"./apps/wekks_calendar_display/imgs/fav_icon.svg",
+		"https://mahmoud46.github.io/Rush_quiz/imgs/fav-icon.svg",
+		"./static/imgs/mushaf_icon.svg",
+		"./apps/ethereye_cryptocurrencies_price_tracker/imgs/fave-icon.svg",
+		"https://raw.githubusercontent.com/Mahmoud46/Shoot_and_Score_ball_kick_simple_simulation/59d901e3a1f3f81180d382dec4aa669a46fe79d3/images/fav-icon.svg",
+	],
+	span_texts = [
+		"Calculo",
+		"Clk",
+		"Cloudi",
+		"Croppit",
+		"Ding",
+		"Exchnge",
+		"Exprot",
+		"Eztext",
+		"Icode",
+		"Imagz",
+		"Keypass",
+		"Klik",
+		"My clock X",
+		"Qrify",
+		"Salat",
+		"Sayinz",
+		"Scalinng",
+		"Scrabs",
+		"Sketshh",
+		"Snapzio",
+		"Tabb",
+		"Novi",
+		"Tranzlate",
+		"Up Vue",
+		"Vi vid cash",
+		"Vibs",
+		"Wekks",
+		"Rush quiz",
+		"Mushaf",
+		"EtherEye",
+		"Shoot&Score",
+	],
+	app_videos = [
+		"./static/videos/Calculo.mp4",
+		"./static/videos/Clk.mp4",
+		"./static/videos/Cloudi.mp4",
+		"./static/videos/Croppit.mp4",
+		"./static/videos/Ding.mp4",
+		"./static/videos/Exchnge.mp4",
+		"./static/videos/Exprot.mp4",
+		"./static/videos/Eztext.mp4",
+		"./static/videos/Icode.mp4",
+		"./static/videos/Imagz.mp4",
+		"./static/videos/Keypass.mp4",
+		"./static/videos/Klik.mp4",
+		"./static/videos/My Clock X.mp4",
+		"./static/videos/Qrify.mp4",
+		"./static/videos/Salat.mp4",
+		"./static/videos/Sayinz.mp4",
+		"./static/videos/Scalinng.mp4",
+		"./static/videos/Scrabz.mp4",
+		"./static/videos/Sketshh.mp4",
+		"./static/videos/Snapzio.mp4",
+		"./static/videos/Tabb.mp4",
+		"./static/videos/Novi.mp4",
+		"./static/videos/Tranzlate.mp4",
+		"./static/videos/Up Vue.mp4",
+		"./static/videos/Vi Vid Cash.mp4",
+		"./static/videos/Vibs.mp4",
+		"./static/videos/Wekks.mp4",
+		"./static/videos/Rush Quiz.mp4",
+		"./static/videos/Mushaf.mp4",
+		"./static/videos/EtherEye.mp4",
+		"./static/videos/Shoot&Score.mp4",
+	];
+
+document.querySelectorAll(".apps-cont a").forEach((btn) =>
+	btn.addEventListener("click", (e) => {
+		e.preventDefault();
+		activateAppWindow(Number(btn.getAttribute("num")));
+	})
+);
+
+document.querySelector(".close_btn").addEventListener("click", (_) => {
+	document.querySelector(".app_window_container").classList.remove("active");
+	document.querySelector(".app_window_container .demo_display video").src =
+		"./static/videos/Aplica_replacement.mp4";
+});
+
+function activateAppWindow(app_num) {
+	document.querySelector(".app_window_container .app_logo img").src =
+		image_srcs[app_num];
+	document.querySelector(".app_window_container .app_logo span").innerText =
+		span_texts[app_num];
+
+	document.querySelector(".app_window_container .app_btns a.demo_view").href =
+		hrefs[app_num];
+
+	document.querySelector(".app_window_container .demo_display video").src =
+		app_videos[app_num];
+
+	document.querySelector(".app_window_container").classList.add("active");
+}
